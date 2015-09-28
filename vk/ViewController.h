0d67e111d179cb87944e97812f7da17c2d4951ee
–Ш–≥–Њ—А—М –Т–µ–і–µ–љ–µ–µ–≤ <igorv@mac-mini-igor.local>
2015-09-28 23:29:15 +0300

    Initial Commit

---
+++ vk/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  vk
+//
+//  Created by Игорь Веденеев on 28.09.15.
+//  Copyright © 2015 Игорь Веденеев. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
