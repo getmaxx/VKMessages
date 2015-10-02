0922d8d06651ca634b67e5e3b3d2ad2bb2d08397
–Ш–≥–Њ—А—М –Т–µ–і–µ–љ–µ–µ–≤ <igorv@mac-mini-igor.local>
2015-10-02 16:37:01 +0300

    status bar

--- vk/AppDelegate.m
+++ vk/AppDelegate.m
@@ -19,7 +19,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    
+    [[UIApplication sharedApplication] setStatusBarStyle: UIStatusBarStyleLightContent];
         
     return YES;
 }
